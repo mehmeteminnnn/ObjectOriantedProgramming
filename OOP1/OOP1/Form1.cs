192c4bb1c2dea6d6b7c4174805b00c7f0188b826
Mehmet Emin TOK <95161664+mehmeteminnnn@users.noreply.github.com>
2022-11-11 14:06:18 +0300

    OOP'ye Başlangıç

---
+++ OOP1/OOP1/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Otomobil oto=new Otomobil();
+            oto.marka = "Ford";
+            oto.model = "Focus";
+            oto.modelyılı = 2015;
+            oto.renk = "Beyaz";
+            oto.vitestipi = "Manuel";
+            string marka = oto.markagetir();
+            label6.Text=oto.marka.ToString();
+            label7.Text=oto.model.ToString();
+            label8.Text = oto.modelyılı.ToString();
+            label9.Text = oto.renk.ToString();
+            label10.Text = oto.vitestipi.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OOP1/OOP1/Otomobil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Otomobil
+    {
+        public string marka { get; set; }
+        public string model { get; set; }   
+        public int modelyılı { get; set; }   
+        public string renk { get; set; }
+        public string vitestipi { get; set; }
+        public string markagetir()
+        { return marka; }
+    }
+}
